7707e3f3024caea9f063bbf2268e78de91f8690a
abdallataha031225 <abdallataha031225@gmail.com>
2026-08-13 00:32:58 +0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,7 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-import "./.next/types/routes.d.ts";
-import "./.next/types/root-params.d.ts";
+import type { NextConfig } from "next";
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["firebase-admin", "jwks-rsa"],
+};
+
+export default nextConfig;
